0d3675e883fd48aa7b1cc27379b4874aa2a264d3
Priyanshujain22 <priyanshujain@jklu.edu.in>
2026-02-17 18:30:37 +0530

    ef

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'login.html'),
+                register: resolve(__dirname, 'register.html'),
+                citizenDashboard: resolve(__dirname, 'citizen-dashboard.html'),
+                adminDashboard: resolve(__dirname, 'admin-dashboard.html'),
+                officerDashboard: resolve(__dirname, 'officer-dashboard.html'),
+                complaintForm: resolve(__dirname, 'complaint-form.html'),
+            }
+        }
+    }
+});
